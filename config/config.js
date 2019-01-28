da1532fd2ed5ff9934ab9f2da8554f91a5c69f0b
Erik Davtyan <erikdavtyan@gmail.com>
2019-01-28 23:39:44 +0400

    Implement correct env var for seeding outside of docker

--- config/config.js
+++ config/config.js
@@ -16,6 +16,13 @@ module.exports = {
     dialect: 'postgres',
     operatorsAliases: false
   },
-  production: {
+  local: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    operatorsAliases: false
   }
 };
